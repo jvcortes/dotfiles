3671397726be06146fca5326294f334d72cc04fc
Javier Cortés <jfelipefc@gmail.com>
2025-04-01 01:00:01 -0500

    work dotfiles

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -4,7 +4,6 @@ export BROWSER="$(which firefox)"
 export PATH="$HOME/scripts:$HOME/.local/bin:$HOME/.node_modules/bin:$HOME/.go/bin:$HOME/.pyenv/bin:$HOME/.cargo/bin:$HOME/.config/emacs/bin:$PATH"
 
 export npm_config_prefix=~/.node_modules
-export LS_COLORS='di=32:ln=36:so=37;41:pi=33:ex=1;30:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
 # Colors for less and man pages
 export LESS_TERMCAP_mb=$'\E[01;32m'
